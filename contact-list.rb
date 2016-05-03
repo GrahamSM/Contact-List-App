51120b5394a8c26da8f5c6436ce84c64fad21cdf
Graham Stirling-Moffet <graham.sm94@gmail.com>
2016-05-03 10:43:38 -0400

    Committed updates

---
+++ contact-list.rb
@@ -0,0 +1,44 @@
+require_relative 'contact'
+
+# Interfaces between a user and their contact list. Reads from and writes to standard I/O.
+class ContactList
+
+  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  puts "Here is a list of available commands: "
+  puts "\tnew\t- Create a new contact"
+  puts "\tlist\t- List all contacts"
+  puts "\tshow ID\t- Show a contact with particular ID"
+  puts "\tsearch\t- Search contacts"
+
+  argv = ARGV
+  contacts = Contact.all
+    case argv[0]
+      when /list/
+        num = 1
+        contacts.each do |contact|
+          puts "#{num}: #{contact.name} (#{contact.email})"
+          num+=1
+        end
+        puts "#{num} records total"
+      when /new/
+        puts "Contact creator..."
+        puts "Please enter a full name for the user"
+        name = STDIN.gets.chomp #?
+        puts "Please enter an email address for the user"
+        email = STDIN.gets.chomp #?
+        Contact.create(name,email)
+      when /show/
+        contact = Contact.find(argv[1].to_i)
+        if contact
+          puts "Contact name: #{contact.name}"
+          puts "Contact e-mail: #{contact.email}"
+        else
+          puts "Contact not found"
+        end
+      when /search/
+        contact = Contact.search(argv[1])
+      else
+        puts "Sorry, command not recognized"
+      end
+
+end
--- contact.rb
+++ contact.rb
@@ -1,16 +1,19 @@
 require 'csv'
+require 'pry'
 
 # Represents a person in an address book.
 # The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
   attr_accessor :name, :email
-
+  @contact_file = "./contacts.csv"
   # Creates a new contact object
   # @param name [String] The contact's name
   # @param email [String] The contact's email address
   def initialize(name, email)
     # TODO: Assign parameter values to instance variables.
+    @name = name
+    @email = email
   end
 
   # Provides functionality for managing contacts in the csv file.
@@ -19,28 +22,47 @@ class Contact
     # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
     # @return [Array<Contact>] Array of Contact objects
     def all
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+      contacts = []
+      File.readlines(@contact_file).each do |line|
+        contact = line.split(', ')
+        contacts << Contact.new(contact[0], contact[1])
+      end
+      return contacts
     end
 
     # Creates a new contact, adding it to the csv file, returning the new contact.
     # @param name [String] the new contact's name
     # @param email [String] the contact's email
     def create(name, email)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+      contact = Contact.new(name, email)
+      File.open(@contact_file, 'a') do |file|
+        file.puts name + ", " + email
+      end
+      num = 0
+      File.readlines(@contact_file).each do |i|
+        num+=1
+      end
+      puts "Contact created successfully with ID #{num}"
     end
 
     # Find the Contact in the 'contacts.csv' file with the matching id.
     # @param id [Integer] the contact id
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+      contacts = Contact.all
+      binding.pry
+      contact = contacts[id]
     end
-
     # Search for contacts by either name or email.
     # @param term [String] the name fragment or email fragment to search for
     # @return [Array<Contact>] Array of Contact objects.
     def search(term)
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+      contacts = Contact.all
+      contacts.each do |contact|
+        if contact.name[term] || contact.email[term]
+          puts "#{contact.name} (#{contact.email})"
+        end
+      end
     end
 
   end
